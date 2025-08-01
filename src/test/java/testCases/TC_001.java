39f2eee6f31c29442445aaeab4c84105b09b6266
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-08-01 23:24:22 +0530

    Update TC_001.java

--- src/test/java/testCases/TC_001.java
+++ src/test/java/testCases/TC_001.java
@@ -10,32 +10,6 @@ public class TC_001 extends BaseClass {
 	
 	@Test(priority=1, groups = { "Sanity", "Regression" })
 	void testSearchAddAndVerifyCart() {
-
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
 		
 		try {
 		
