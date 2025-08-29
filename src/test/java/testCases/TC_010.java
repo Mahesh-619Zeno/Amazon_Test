d28ade10aeb6a7942bcc7200bb4ace7ff89216f3
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-08-29 17:36:49 +0530

    feat: removed unused files and updated code

--- src/test/java/testCases/TC_010.java
+++
@@ -1,9 +0,0 @@
-@Test
-public void testLoginInvalidCredentials() {
-    LoginService loginService = new LoginService();
-    
-    // Assuming the system has a method for verifying credentials
-    boolean result = loginService.login("invalidUser", "wrongPassword");
-    
-    assertFalse(result, "Login should fail with invalid credentials.");
-}
